fd2c0aa337b6399f69b518a3fecf11fe8389019b
mohammedabdelraouf <mohammed20191700557@cis.asu.edu.eg>
2024-07-26 20:35:24 +0300

    first commit of final project

---
+++ Bouns-task/scripts/myScript.js
@@ -0,0 +1,39 @@
+// var fName = prompt("Enter Your first name" , "");
+// console.log(fName)
+// console.log("input type is :" + typeof(fName)) ;
+
+// (fName) ? console.log("correct") : console.log("wrong"); 
+
+const result =document.getElementById("result");
+var Degree = prompt("Enter Your degree" );
+  
+Degree = Number(Degree)
+
+if(!Degree){
+    result.append("please enter a valid number")
+    console.log("please enter a valid number")
+} 
+else if(Degree >= 90 && Degree<=100){
+    result.append("your Grade is : A")
+    console.log("your Grade is : A")
+}
+else if(Degree >= 80 && Degree < 90){
+    result.append("your Grade is : B")
+    console.log("your Grade is : B")
+}
+else if(Degree >= 70 && Degree < 80){
+    result.append("your Grade is : C")
+    console.log("your Grade is : C")
+}
+else if(Degree >= 60 && Degree < 70){
+    result.append("your Grade is : D")
+    console.log("your Grade is : D")
+}
+else if( Degree >100 ){
+    result.append("Enter your Degree from 100")
+    console.log("Enter your Degree from 100")
+}
+else if(Degree < 60){
+    result.append("your Grade is : F   WORK HARD To get success ")
+    console.log("your Grade is : F   WORK HARD To get success " + Degree)
+}
